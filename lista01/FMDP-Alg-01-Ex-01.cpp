92c34c9792abff11c6d8f428c7b9c41327a2e2db
Fábio Miguel <fabiomigueldp@gmail.com>
2024-03-12 03:52:10 -0300

    teste GPG key

---
+++ lista01/FMDP-Alg-01-Ex-01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Fábio Miguel" << std::endl;
+    std::cout << "Rua Rene Zamlutti, 94" << std::endl;
+    std::cout << "Chácara Klabin" << std::endl;
+    std::cout << "São Paulo - SP" << std::endl;
+
+    return 0;
+}
